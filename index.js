59008d4994506a988e0669f92ac40ee0ad0eb05c
Nils <nilsnathorstwindahl@gmail.com>
2018-12-02 14:18:55 +0100

    added randomizer function

--- index.js
+++ index.js
@@ -24,32 +24,58 @@ const cardsArray = [{
   },
 ];
 
+
+const shuffledArray = [];
 const cardElts = [...document.querySelectorAll('.card')];
 const imageTag = [...document.querySelectorAll('.cardImg')];
-
 let current, previous;
 
-cardElts.forEach((card, i) => {
-  const len = i%(cardsArray.length);
-  const img = cardsArray[len];
-  imageTag[i].setAttribute("src", "images/"+img.img);
+function shuffle(array) {
+  for (var i = 0; i < array.length; i++) {
+    let rand = Math.floor(Math.random() * array.length);
+    if (!shuffledArray.includes(rand)) {
+      shuffledArray.push(rand);
+    } else {
+      i--;
+    }
+  }
+  return shuffledArray;
+}
 
-  card.dataset.id = len+1;
+shuffle(cardsArray);
+
+
+cardElts.forEach((card, i) => {
+  const len = i % (cardsArray.length);
+  const img = cardsArray[len];
+  imageTag[i].setAttribute("src", "images/" + img.img);
+
+  card.dataset.id = len + 1;
   const cardId = card.dataset.id;
 
   //handle clicks
-  card.addEventListener('click', () => {
-    console.log('i was clicked '+cardId);
-  //handle matches
-  previous = current;
-  current = cardId;
-  if (previous == cardId) {
-    console.log('match')
-    let matches = document.querySelectorAll(`[data-id="${previous}"]`);
-    matches.forEach(matched => {
-      matched.classList.add('matched');
-    })
-  }
-  })
+  card.addEventListener('click', handleClick);
 
+
+  function handleClick() {
+    card.classList.add('clicked');
+    console.log('i was clicked ' + cardId);
+    //handle matches
+    previous = current;
+    current = cardId;
+    if (previous == cardId) {
+      console.log('match')
+      let matches = document.querySelectorAll(`[data-id="${previous}"]`);
+      matches.forEach(matched => {
+        matched.classList.add('matched');
+        matched.removeEventListener('click', handleClick);
+      })
+    } else if (previous !== cardId && current !== previous) {
+
+      let nonMatches = document.querySelectorAll(`[data-id="${previous}"]`);
+      nonMatches.forEach(notMatched => {
+        notMatched.classList.remove('clicked');
+      })
+    }
+  }
 });
