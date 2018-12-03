c3b30fa871f684d261586d744e122e4d159306f4
Nils <nilsnathorstwindahl@gmail.com>
2018-12-03 12:19:47 +0100

    added fart gifs

--- data.js
+++ data.js
@@ -1,40 +1,16 @@
-const cardsArray = [{
-    img: "images/1.png"
-  },
-  {
-    img: "images/2.png"
-  },
-  {
-    img: "images/3.png"
-  },
-  {
-    img: "images/4.png"
-  },
-  {
-    img: "images/5.png"
-  },
-  {
-    img: "images/6.png"
-  },
-  {
-    img: "images/7.png"
-  },
-  {
-    img: "images/8.png"
-  },
-]
 
   var api = "https://api.giphy.com/v1/gifs/search?";
   var apiKey = "&api_key=9kJfRi6ip66K2xkxwSKa7ZAyK7H5sjpY";
-  var query = "&q=memory";
+  var query = "&q=fart";
   var url = api + apiKey + query;
 
   fetch(url).then(response => {
     return response.json()
   }).then(response => {
-    for (var i = 0; i < cardsArray.length; i++) {
+    let gifArray = [];
+    for (var i = 0; i < 8; i++) {
       result = response.data[i].images.downsized.url
-      console.log(result);
-      // cardsArray.push(result);
+        gifArray.push(result);
     }
+  startGame(gifArray);
   })
--- index.js
+++ index.js
@@ -1,63 +1,83 @@
-const idArray = [0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7]
-let previous, current
-let isRevelead, disable = false
-const cards = [...document.querySelectorAll('.card')]
-const flippedCards = [...document.querySelectorAll('.flipped')]
 
-function shuffle(array) {
-  var currentIndex = array.length,
-    temporaryValue, randomIndex
-  while (0 !== currentIndex) {
-    randomIndex = Math.floor(Math.random() * currentIndex)
-    currentIndex -= 1
-    temporaryValue = array[currentIndex]
-    array[currentIndex] = array[randomIndex]
-    array[randomIndex] = temporaryValue
+
+function startGame(images) {
+console.log(images)
+  const gameBoard = document.querySelector(".game-board");
+
+  for (var i = 0; i < images.length; i++) {
+    gameBoard.innerHTML += createCards(images, i);
   }
-  return array
-}
-shuffle(idArray)
+  const idArray = [0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7]
+  let previous, current
+  let isRevelead, disable = false
+  const cards = [...document.querySelectorAll('.card')]
+  const flippedCards = [...document.querySelectorAll('.flipped')]
 
+  function createCards(array, i) {
+    return `<div class="card" data-id="">
+    <img class="default" src="/images/default.png" />
+    <img class="flipped" src=""/>
+    </div>
+    <div class="card" data-id="">
+    <img class="default" src="/images/default.png" />
+    <img class="flipped" src=""/>
+    </div>`
+  }
+
+  function shuffle(array) {
+    var currentIndex = array.length,
+    temporaryValue, randomIndex
+    while (0 !== currentIndex) {
+      randomIndex = Math.floor(Math.random() * currentIndex)
+      currentIndex -= 1
+      temporaryValue = array[currentIndex]
+      array[currentIndex] = array[randomIndex]
+      array[randomIndex] = temporaryValue
+    }
+    return array
+  }
+
+  function handleClick() {
+    if (disable || this === previous) return
+    this.classList.add('flip')
+    if (!isRevelead) {
+      isRevelead = true
+      previous = this
+      return
+    }
+    current = this
+    previous.dataset.id === current.dataset.id ? killCard() : resetCards()
+  }
+
+  function killCard() {
+    previous.removeEventListener('click', handleClick)
+    current.removeEventListener('click', handleClick)
+    clear()
+  }
+
+  function resetCards() {
+    disable = true
+    setTimeout(() => {
+      previous.classList.remove('flip')
+      current.classList.remove('flip')
+      clear()
+    }, 800)
+  }
+
+  function clear() {
+    isRevelead = false
+    disable = false
+    previous = null
+    current = null
+  }
+  shuffle(idArray)
 
   cards.forEach((card, i) => {
     card.addEventListener('click', handleClick)
     let randNum = idArray[i]
-    randomImage = cardsArray[randNum].img
+    randomImage = images[randNum]
     card.dataset.id = randNum + 1
     cardId = card.dataset.id
     flippedCards[i].setAttribute("src", randomImage)
   })
-
-function handleClick() {
-  if (disable || this === previous ) return
-  this.classList.add('flip')
-  if (!isRevelead) {
-    isRevelead = true
-    previous = this
-    return
-  }
-  current = this
-  previous.dataset.id === current.dataset.id ? killCard() : resetCards()
-}
-
-function killCard() {
-  previous.removeEventListener('click', handleClick)
-  current.removeEventListener('click', handleClick)
-  clear()
-}
-
-function resetCards() {
-  disable = true
-  setTimeout(() => {
-    previous.classList.remove('flip')
-    current.classList.remove('flip')
-    clear()
-  }, 800)
-}
-
-function clear() {
-  isRevelead = false
-  disable = false
-  previous = null
-  current = null
 }
