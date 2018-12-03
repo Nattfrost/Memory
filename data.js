3d107393b178e5c06e929c98a40171d6f152068e
Nils <nilsnathorstwindahl@gmail.com>
2018-12-03 15:10:26 +0100

    Added stuff but removed

--- data.js
+++ data.js
@@ -1,14 +1,15 @@
 
   var api = "https://api.giphy.com/v1/gifs/search?";
   var apiKey = "&api_key=9kJfRi6ip66K2xkxwSKa7ZAyK7H5sjpY";
-  var query = "&q=fart";
+  var query = "&q=random";
   var url = api + apiKey + query;
-
   fetch(url).then(response => {
     return response.json()
   }).then(response => {
+
     let gifArray = [];
     for (var i = 0; i < 8; i++) {
+      // console.log(response.data)
       result = response.data[i].images.downsized.url
         gifArray.push(result);
     }
--- index.js
+++ index.js
@@ -1,41 +1,39 @@
+function createCards(array, i) {
+  return `
+<div class="card" data-id="">
+<img class="default" src="/images/default.png" />
+<img class="flipped" src=""/>
+</div>
+<div class="card" data-id="">
+<img class="default" src="/images/default.png" />
+<img class="flipped" src=""/>
+</div>`
+}
 
+function shuffle(array) {
+  var currentIndex = array.length,
+    temporaryValue, randomIndex
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex)
+    currentIndex -= 1
+    temporaryValue = array[currentIndex]
+    array[currentIndex] = array[randomIndex]
+    array[randomIndex] = temporaryValue
+  }
+  return array
+}
 
 function startGame(images) {
-console.log(images)
-  const gameBoard = document.querySelector(".game-board");
-
+  const gameBoard = document.querySelector(".game-board")
   for (var i = 0; i < images.length; i++) {
-    gameBoard.innerHTML += createCards(images, i);
+    gameBoard.innerHTML += createCards(images, i)
   }
+
   const idArray = [0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7]
   let previous, current
   let isRevelead, disable = false
   const cards = [...document.querySelectorAll('.card')]
   const flippedCards = [...document.querySelectorAll('.flipped')]
-
-  function createCards(array, i) {
-    return `<div class="card" data-id="">
-    <img class="default" src="/images/default.png" />
-    <img class="flipped" src=""/>
-    </div>
-    <div class="card" data-id="">
-    <img class="default" src="/images/default.png" />
-    <img class="flipped" src=""/>
-    </div>`
-  }
-
-  function shuffle(array) {
-    var currentIndex = array.length,
-    temporaryValue, randomIndex
-    while (0 !== currentIndex) {
-      randomIndex = Math.floor(Math.random() * currentIndex)
-      currentIndex -= 1
-      temporaryValue = array[currentIndex]
-      array[currentIndex] = array[randomIndex]
-      array[randomIndex] = temporaryValue
-    }
-    return array
-  }
 
   function handleClick() {
     if (disable || this === previous) return
@@ -76,8 +74,10 @@ console.log(images)
     card.addEventListener('click', handleClick)
     let randNum = idArray[i]
     randomImage = images[randNum]
-    card.dataset.id = randNum + 1
-    cardId = card.dataset.id
+    card.dataset.id = randNum
     flippedCards[i].setAttribute("src", randomImage)
   })
+}
+function newGame() {
+  
 }
